5397b966f111849a86048011becca67340871c5d
dev <16483799+legacyswdev@users.noreply.github.com>
2024-05-04 16:12:25 -0400

    HHH-NNN JPA CTE query issue
    JPA query with CTE does not work but cross join works.
    
    https://stackoverflow.com/questions/78004995/cte-usage-with-spring-data-jpa-3-2-hibernate-6-4-does-not-work
    
    example
    try (Session s = openSession()) {
                s.createQuery( "with c1 as (select c.id as id from CteEntity c), " +
                                "c2 as (select c.id as id from CteEntity c)" +
                                "  select c from CteEntity c" +
                                " join c1 on c.id = c1.id " +
                                " join c2 on c.id = c2.id" )
                        .list();
            }
    
    exception is
    
    java.lang.IllegalArgumentException: org.hibernate.query.SemanticException: Could not interpret path expression 'c1.id'
    
    	at org.hibernate.internal.ExceptionConverterImpl.convert(ExceptionConverterImpl.java:143)
    	at org.hibernate.internal.ExceptionConverterImpl.convert(ExceptionConverterImpl.java:167)
    	at org.hibernate.internal.ExceptionConverterImpl.convert(ExceptionConverterImpl.java:173)
    	at org.hibernate.internal.AbstractSharedSessionContract.createQuery(AbstractSharedSessionContract.java:860)
    	at org.hibernate.internal.AbstractSharedSessionContract.createQuery(AbstractSharedSessionContract.java:765)
    	at org.hibernate.internal.AbstractSharedSessionContract.createQuery(AbstractSharedSessionContract.java:140)
    	at org.hibernate.bugs.JpaCteTest.testCteJoinClause(JpaCteTest.java:14)
    	at java.base/jdk.internal.reflect.DirectMethodHandleAccessor.invoke(DirectMethodHandleAccessor.java:104)
    	at java.base/java.lang.reflect.Method.invoke(Method.java:578)
    	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)
    	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
    	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:56)
    	at org.hibernate.testing.junit4.ExtendedFrameworkMethod.invokeExplosively(ExtendedFrameworkMethod.java:45)
    	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
    	at org.junit.internal.runners.statements.RunBefores.evaluate(RunBefores.java:26)
    	at org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)
    	at org.junit.internal.runners.statements.FailOnTimeout$CallableStatement.call(FailOnTimeout.java:299)
    	at org.junit.internal.runners.statements.FailOnTimeout$CallableStatement.call(FailOnTimeout.java:293)
    	at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:317)
    	at java.base/java.lang.Thread.run(Thread.java:1589)
    Caused by: org.hibernate.query.SemanticException: Could not interpret path expression 'c1.id'
    	at org.hibernate.query.hql.internal.BasicDotIdentifierConsumer$BaseLocalSequencePart.resolvePathPart(BasicDotIdentifierConsumer.java:241)

---
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPACteTestCase.java
@@ -0,0 +1,52 @@
+package org.hibernate.bugs;
+
+import jakarta.persistence.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test of JPA/JQL common table expression (CTE)
+ * https://in.relation.to/2023/02/20/hibernate-orm-62-ctes/
+ *
+ * @author legacyswdev
+ */
+public class JPACteTestCase  {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+    }
+
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void testCteJoinClause() {
+        entityManagerFactory.createEntityManager().createQuery( "with c1 as (select c.id as id from CteEntity c), " +
+                        "c2 as (select c.id as id from CteEntity c)" +
+                        "  select c from CteEntity c" +
+                        " join c1 on c.id = c1.id " +
+                        " join c2 on c.id = c2.id" )
+                .getResultList();
+    }
+
+    @Test
+    public void testCteJoinCross() {
+        entityManagerFactory.createEntityManager().createQuery( "select c1 from CteEntity c1,(select c.id as id from CteEntity c) c2," +
+                        "       (select c.id as id from CteEntity c) c3" +
+                        " where c1.id = c2.id and c2.id = c3.id" )
+                .getResultList();
+    }
+
+    @Entity(name = "CteEntity")
+    public static class CteEntity{
+        @Id
+        long id;
+    }
+}
